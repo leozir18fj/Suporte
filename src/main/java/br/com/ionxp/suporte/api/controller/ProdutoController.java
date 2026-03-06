22cea8ce658b75cf4b7db0c154279d768437124c
Leozir <leozir18fj@yahoo.com.br>
2026-03-05 22:19:47 -0300

    incrementado o repository, metodos de busca

--- src/main/java/br/com/ionxp/suporte/api/controller/ProdutoController.java
+++ src/main/java/br/com/ionxp/suporte/api/controller/ProdutoController.java
@@ -2,18 +2,18 @@ package br.com.ionxp.suporte.api.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ionxp.suporte.domain.model.Produto;
 import br.com.ionxp.suporte.domain.repository.ProdutoRepository;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 @RestController
 public class ProdutoController {
 	
-	@Autowired
-	private ProdutoRepository produtoRepository;
+	private final ProdutoRepository produtoRepository;
 	
 	@GetMapping("/produtos")
 	public List<Produto> listar(){
--- src/main/java/br/com/ionxp/suporte/domain/repository/ProdutoRepository.java
+++ src/main/java/br/com/ionxp/suporte/domain/repository/ProdutoRepository.java
@@ -1,5 +1,7 @@
 package br.com.ionxp.suporte.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,8 @@ import br.com.ionxp.suporte.domain.model.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	List<Produto> findByDescricao(String nome);
+	List<Produto> findByDescricaoContaining(String nome);
 
 }
