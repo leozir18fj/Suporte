b27e0623429a03283f2dca56a4855587b33d9648
Leozir <leozir18fj@yahoo.com.br>
2026-03-07 20:35:23 -0300

    PESQUISA POR ID E SALVAR PRODUTO NO BD

--- src/main/java/br/com/ionxp/suporte/api/controller/ProdutoController.java
+++ src/main/java/br/com/ionxp/suporte/api/controller/ProdutoController.java
@@ -2,7 +2,12 @@ package br.com.ionxp.suporte.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ionxp.suporte.domain.model.Produto;
@@ -11,16 +16,31 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/produtos")
 public class ProdutoController {
 	
 	private final ProdutoRepository produtoRepository;
 	
-	@GetMapping("/produtos")
+	@GetMapping
 	public List<Produto> listar(){
 		
 		return produtoRepository.findAll();
+				
+	}
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> buscar(@PathVariable Long id) {
+		return produtoRepository.findById(id).map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 		
-		
+//		if (optional.isPresent()) {
+//			return ResponseEntity.ok(optional.get());
+//		}
+//		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public Produto adicionar(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
 	}
 
 }
