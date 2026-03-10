2a10e5519548025d100fd6f302592d6c12548dac
Leozir <leozir18fj@yahoo.com.br>
2026-03-09 22:38:33 -0300

    editar e excluir produto

--- src/main/java/br/com/ionxp/suporte/api/controller/ProdutoController.java
+++ src/main/java/br/com/ionxp/suporte/api/controller/ProdutoController.java
@@ -3,9 +3,11 @@ package br.com.ionxp.suporte.api.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,5 +44,27 @@ public class ProdutoController {
 	public Produto adicionar(@RequestBody Produto produto) {
 		return produtoRepository.save(produto);
 	}
-
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Produto> atualizar(@PathVariable Long id, @RequestBody Produto produto) {
+		
+		if (!produtoRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		produto.setId(id);
+		Produto produtoAtualizado = produtoRepository.save(produto);
+		return ResponseEntity.ok(produtoAtualizado);
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> remover(@PathVariable Long id) {
+		if (!produtoRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		produtoRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
 }
